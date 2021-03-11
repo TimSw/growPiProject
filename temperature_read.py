e7d7ce17b539e44dc82386fcd975708b27df2496
timswinnen <Tim.Swinnen@iCloud.com>
2021-03-11 19:09:57 +0100

    RotatingFileHandler

--- temperature_read.py
+++ temperature_read.py
@@ -1,8 +1,30 @@
 #!/usr/bin/env python3
+import logging
+import logging.handlers
 import sqlite3
 import time
 from w1thermsensor import W1ThermSensor
 
+# Initialise logger
+# create logger with "temperature_read"
+logger = logging.getLogger("temperature_read")
+logger.setLevel(logging.DEBUG)
+# create file handler which logs even debug messages
+rfh = logging.handlers.RotatingFileHandler("temperature_read.log", "a",
+                                           2560000, 3)
+rfh.setLevel(logging.DEBUG)
+# create console handler with a higher log level
+ch = logging.StreamHandler()
+ch.setLevel(logging.DEBUG)
+# ch.setLevel(logging.ERROR)
+# create formatter and add it to the handlers
+formatter = logging.Formatter(
+   "%(asctime)s - %(name)s - %(levelname)s - %(lineno)d: %(message)s")
+rfh.setFormatter(formatter)
+ch.setFormatter(formatter)
+# add the handlers to the logger
+logger.addHandler(rfh)
+logger.addHandler(ch)
 # Define data.db directory
 data_db = '/home/pi/growPiProject/data.db'
 
@@ -19,23 +41,23 @@ cur.execute('''CREATE TABLE IF NOT EXISTS temperature
 try:
     sensor1 = W1ThermSensor(W1ThermSensor.THERM_SENSOR_DS18B20, "800000265db7")
 except Exception as e:
-    print(e)
+    logger.exception(e)
 try:
     sensor2 = W1ThermSensor(W1ThermSensor.THERM_SENSOR_DS18B20, "041662e87fff")
 except Exception as e:
-    print(e)
+    logger.exception(e)
 try:
     sensor3 = W1ThermSensor(W1ThermSensor.THERM_SENSOR_DS18B20, "80000026d9bf")
 except Exception as e:
-    print(e)
+    logger.exception(e)
 try:
     sensor4 = W1ThermSensor(W1ThermSensor.THERM_SENSOR_DS18B20, "04166084f8ff")
 except Exception as e:
-    print(e)
+    logger.exception(e)
 try:
     sensor5 = W1ThermSensor(W1ThermSensor.THERM_SENSOR_DS18B20, "041662af5bff")
 except Exception as e:
-    print(e)
+    logger.exception(e)
 
 # Initialise temperatures:
 temp1 = 0
@@ -51,23 +73,23 @@ now = time.time()
 try:
     temp1 = sensor1.get_temperature()
 except Exception as e:
-    print(e)
+    logger.exception(e)
 try:
     temp2 = sensor2.get_temperature()
 except Exception as e:
-    print(e)
+    logger.exception(e)
 try:
     temp3 = sensor3.get_temperature()
 except Exception as e:
-    print(e)
+    logger.exception(e)
 try:
     temp4 = sensor4.get_temperature()
 except Exception as e:
-    print(e)
+    logger.exception(e)
 try:
     temp5 = sensor5.get_temperature()
 except Exception as e:
-    print(e)
+    logger.exception(e)
 
 # Insert a row of data
 cur.execute("INSERT INTO temperature VALUES (?, ?, ?, ?, ?, ?)",
